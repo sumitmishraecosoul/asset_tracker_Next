54f795ce87b9fb8917a3b05dc0a1e02e7b82727a
Harshit Kant <Harshit.Kant@ecosoulhome.com>
2025-10-30 15:18:18 +0530

    domain changed

--- config/config.js
+++ config/config.js
@@ -1,4 +1,4 @@
 // const DEV_URL = "http://localhost:5051";
 
-const DEV_URL = "http://api.asset.thrivebrands-hrms.thrivebrands.ai";
+const DEV_URL = "https://api.asset.thrivebrands-hrms.thrivebrands.ai";
 export default DEV_URL ;
